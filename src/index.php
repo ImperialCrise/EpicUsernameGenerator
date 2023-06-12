e0713a482945816cbda688d1319e02e090e4115e
Emre ULUSOY <emre.ulusoy@epita.fr>
2023-06-12 20:44:04 +0200

    source codes

---
+++ src/index.php
@@ -0,0 +1,66 @@
+<?php
+
+include 'nouns.php';
+include 'adjectives.php';
+
+function epicUsernameGenerator($config)
+{
+    if (!isset($config['dictionaries'])) {
+        throw new Exception("Cannot find any dictionary. Please provide at least one language dictionary");
+    } else {
+        $dictionariesLength = count($config['dictionaries']);
+        $name = '';
+        for ($i = 0; $i < $dictionariesLength; $i++) {
+            if ($name && isset($config['separator']) ) {
+                $randomIndex = rand(0, (count($config['dictionaries'][$i])-1));
+                $name .= ($config['separator'] ? $config['separator'] : '') . $config['dictionaries'][$i][$randomIndex];
+            } else {
+                $randomIndex = rand(0, (count($config['dictionaries'][$i])-1));
+                $randomName = $config['dictionaries'][$i][$randomIndex];
+                $nameLength = rand(4-$i, strlen($randomName)/2);
+                $name .= substr($randomName, 0, $nameLength);
+            }
+        }
+
+        $username = $name . randomNumber($config['randomDigits']);
+
+        $username = strtolower($username);
+
+        if (isset($config['style'])) {
+            $style = $config['style'];
+            if ($style === 'lowerCase') {
+                $username = strtolower($username);
+            } else if ($style === 'capital') {
+                $username = ucfirst($username);
+            } else if ($style === 'upperCase') {
+                $username = strtoupper($username);
+            }
+        }
+
+        if (isset($config['length'])) {
+            $username = substr($username, 0, $config['length']);
+        } else {
+            $username = substr($username, 0, 15);
+        }
+
+        return $username;
+    }
+}
+
+// Example
+$englishConfig = [
+    'dictionaries' => [
+        $nouns,
+        $adjectives
+    ],
+    'randomDigits' => 6,
+    'length' => 8,
+    'style' => 'lowerCase'
+];
+
+for ($a=0; $a < 30; $a++) { 
+    $username = epicUsernameGenerator($config);
+    echo $username . "<br>";
+}
+
+?>
